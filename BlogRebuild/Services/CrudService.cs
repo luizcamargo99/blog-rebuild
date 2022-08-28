0acb331cbaddec1f93b15a23c4f6c483469160fc
Luiz Camargo <luiz.zito@hotmail.com>
2022-08-28 16:52:38 +0100

    updates

--- BlogRebuild/Services/CrudService.cs
+++ BlogRebuild/Services/CrudService.cs
@@ -11,23 +11,10 @@ namespace BlogRebuild.Services
         {
             _httpClient = httpClient;
         }
-
-        public async Task<HttpResponseMessage> Post(string endpoint, string jsonString)
-        {
-            return await _httpClient.PostAsync(string.Concat(Urls.ApiBlogAdmin, endpoint), ConvertStringToStringContent(jsonString));
-        }
         public async Task<HttpResponseMessage> Get(string endpoint, string query = "")
         {
             
             return await _httpClient.GetAsync(string.Concat(Urls.ApiBlogAdmin, endpoint, query));
-        }
-        public async Task<HttpResponseMessage> Put(string endpoint, string jsonString)
-        {
-            return await _httpClient.PutAsync(string.Concat(Urls.ApiBlogAdmin, endpoint), ConvertStringToStringContent(jsonString));
-        }
-        public async Task<HttpResponseMessage> Delete(string query)
-        {
-            return await _httpClient.DeleteAsync(query);
         }
 
         private StringContent ConvertStringToStringContent(string jsonString)
