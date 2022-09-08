38475ac70a764ebfee84d35ab1b448524b096bf6
Luiz Camargo <luiz.zito@hotmail.com>
2022-09-08 15:50:58 +0100

    updates

--- BlogRebuild/Services/CrudService.cs
+++ BlogRebuild/Services/CrudService.cs
@@ -1,15 +1,10 @@
 ﻿using BlogRebuild.Constants;
-using Newtonsoft.Json.Linq;
-using System.Net.Http.Headers;
-using System.Net.Mime;
-using System.Text;
 
 namespace BlogRebuild.Services
 {
     public class CrudService
     {
         private readonly HttpClient _httpClient;
-        private const string _mediaType = "application/json";
 
         public CrudService(HttpClient httpClient)
         {
@@ -18,11 +13,6 @@ namespace BlogRebuild.Services
         public async Task<HttpResponseMessage> Get(string endpoint, string query = "")
         {
             return await _httpClient.GetAsync(string.Concat(Urls.ApiBlogAdmin, endpoint, query));
-        }
-
-        public StringContent ConvertStringToStringContent(string jsonString)
-        {
-            return new StringContent(jsonString, System.Text.Encoding.UTF8, _mediaType);
         }
     }
 }
